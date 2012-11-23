3c623b3a237a4c3cbb03eeedf858edef98ed0465
stefan.marks.ac <stefan.marks.ac@gmail.com>
2012-11-23 03:05:25 +0000

    - Added multicolour LED support - Modified Unity script to deal with completely missing Arduino board

---
+++ Arduino_JetBlackIO/MulticolourLED.cpp
@@ -0,0 +1,65 @@
+/**
+ * Multicolour LED class implementation.
+ *
+ * @version 1.0 - 2012.11.23: Created
+ * @author  Stefan Marks
+ */
+ 
+#include "MulticolourLED.h"
+
+MulticolourLED::MulticolourLED(LED* pLEDred, LED* pLEDgreen, LED* pLEDblue) : LED()
+{
+  this->pLEDred   = pLEDred;
+  this->pLEDgreen = pLEDgreen;
+  this->pLEDblue  = pLEDblue;
+
+  red   = 0;
+  green = 0;
+  blue  = 0;
+  
+  updateLedState();
+}
+
+
+void MulticolourLED::setColour(byte red, byte green, byte blue)
+{
+  this->red   = constrain(red,   0, 99);
+  this->green = constrain(green, 0, 99);
+  this->blue  = constrain(blue,  0, 99);
+  updateLedState();
+}
+
+
+void MulticolourLED::setBlinkInterval(unsigned int interval)
+{
+  // set all components synchronously
+  if ( pLEDred   != NULL ) pLEDred->setBlinkInterval(interval);
+  if ( pLEDgreen != NULL ) pLEDgreen->setBlinkInterval(interval);
+  if ( pLEDblue  != NULL ) pLEDblue->setBlinkInterval(interval);
+  LED::setBlinkInterval(interval);
+}
+
+
+void MulticolourLED::setBlinkRatio(byte ratio)
+{
+  // set all components synchronously
+  if ( pLEDred   != NULL ) pLEDred->setBlinkRatio(ratio);
+  if ( pLEDgreen != NULL ) pLEDgreen->setBlinkRatio(ratio);
+  if ( pLEDblue  != NULL ) pLEDblue->setBlinkRatio(ratio);
+  LED::setBlinkRatio(ratio);
+}
+
+
+void MulticolourLED::update(unsigned long /* time */)
+{
+  // do nothing here, LED's are updated by themselves
+}
+
+
+void MulticolourLED::updateLedState()
+{
+  if ( pLEDred   != NULL ) pLEDred->setBrightness(  (int) red   * brightness / 99);
+  if ( pLEDgreen != NULL ) pLEDgreen->setBrightness((int) green * brightness / 99);
+  if ( pLEDblue  != NULL ) pLEDblue->setBrightness( (int) blue  * brightness / 99);
+}
+
---
+++ Arduino_JetBlackIO/MulticolourLED.h
@@ -0,0 +1,65 @@
+/**
+ * Class for multicolour LEDs connected to analog pins of the board.
+ * 
+ * @version 1.0 - 2012.11.22: Created
+ * @author  Stefan Marks
+ */
+ 
+#ifndef MULTICOLOUR_LED_H_INCLUDED
+#define MULTICOLOUR_LED_H_INCLUDED
+
+#include "LED.h"
+
+class MulticolourLED : public LED
+{
+  public:
+
+    /**
+     * Creates a multicolour LED class for three separate LEDs.
+     *
+     * @param pLEDred   the LED object that is controlling the red component
+     * @param pLEDgreen the LED object that is controlling the green component
+     * @param pLEDblue  the LED object that is controlling the green component
+     */
+    MulticolourLED(LED* pLEDred, LED* pLEDgreen, LED* pLEDblue);
+
+    /**
+     * Sets the colour of the LED.
+     *
+     * @param red   the red   colour component (0-99)
+     * @param green the green colour component (0-99)
+     * @param blue  the blue  colour component (0-99)
+     */
+    void setColour(byte red, byte green, byte blue);
+
+    /**
+     * Sets the LED blink interval.
+     * 
+     * @param interval blink interval in milliseconds (0: no blinking)
+     */
+    virtual void setBlinkInterval(unsigned int interval);
+    
+    /**
+     * Sets the LED blink ratio.
+     * 
+     * @param ratio on/off blink ratio in percent
+     */
+    virtual void setBlinkRatio(byte ratio);
+    
+    virtual void update(unsigned long time);
+
+  private:
+  
+    virtual void updateLedState();
+
+  private:
+  
+    LED* pLEDred;
+    LED* pLEDgreen;
+    LED* pLEDblue;
+    byte red, green, blue;
+};
+
+
+#endif // MULTICOLOUR_LED_H_INCLUDED
+
